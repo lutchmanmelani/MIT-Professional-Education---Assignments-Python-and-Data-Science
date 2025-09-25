6f0a6b9d8d2560c7e92ede4ce9e76b770160978f
Melani Lutchman <lutchmanmelani@gmail.com>
2025-09-25 18:11:11 +0200

    Update raw_input.py

--- raw_input.py
+++ raw_input.py
@@ -1,1 +1,16 @@
+#Changed to input - using python version 3
+name = input("what is your name?")
+city = input("what city do you live in?")
+state = input("What state is that in?")
 
+print("Hello there! It is so great to meet you")
+#One way to do this is to print strings on separate lines
+print(name)
+print("from")
+print(city)
+print(state)
+#Changed from commas to + signs - using python version 3
+print(name.title()+ " " + "from " + city.title()+ " " + state.title())
+
+age = input("Pardon my rudeness, but how old are you?")
+print("Wow! you look like you could be " + int(age - (0.15 * age)))
